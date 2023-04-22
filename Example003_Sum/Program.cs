5f6d2d842bbcdc58b1294178955de381802c8272
«Valeria» <ldl1@list.ru>
2023-04-22 21:46:02 +0300

    add random construction into the Task Sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
 ﻿// Сумма двух чисел
-int n1 = 9;
-int n2 = -4;
+int n1 = new Random().Next(1,10);
+Console.WriteLine(n1);
+int n2 = new Random().Next(1,10);
+Console.WriteLine(n2);
 Console.WriteLine(n1 + n2);
