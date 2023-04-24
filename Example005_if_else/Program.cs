7005149d7535c17f3608a6c2ff44833eac46605c
«Valeria» <ldl1@list.ru>
2023-04-24 04:00:25 +0300

    add if_else and .ToLower

---
+++ Example005_if_else/Program.cs
@@ -0,0 +1,14 @@
+﻿// Оператор ветвления. Задача на приветствие особенного пользователя
+Console.WriteLine("Введите Ваше имя");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.Write("Да это же ");
+    Console.Write(username);
+    Console.WriteLine("!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
