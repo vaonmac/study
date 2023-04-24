40f029d652ccc30d993c327e6f6342869b9c0098
«Valeria» <ldl1@list.ru>
2023-04-24 04:14:30 +0300

    add Example006_Max

---
+++ Example006_Max/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача с гирями. Нахождение самой тяжелой из пяти
+int a = 11;
+int b = 2;
+int c = 7;
+int d = 8;
+int e = 3;
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write("Максимальный вес - ");
+Console.WriteLine(max);
